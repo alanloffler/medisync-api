8ccfbd937041c0b8dc5f412f8b0469cdfc1ea922
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 19:49:14 -0300

    Created config file for professionals

---
+++ src/common/config/professionals.config.ts
@@ -0,0 +1,11 @@
+export const PROF_CONFIG = {
+  success: {
+    created: 'Profesional creado con éxito',
+    empty: 'No hay profesionales en la base de datos',
+  },
+  errors: {
+    notCreated: 'El profesional no se pudo crear en la base de datos',
+    notFound: 'Profesional no encontrado en la base de datos',
+    notValid: 'ID inválido. La estructura del ID es incorrecta',
+  }
+}
