3fa4e0a60063a35bcec2ab6fc8e1017609233dd8
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 13:21:01 -0300

    Adde class-validator

--- src/professionals/entities/professional.entity.ts
+++ src/professionals/entities/professional.entity.ts
@@ -1,1 +1,42 @@
-export class Professional {}
+import { Transform } from "class-transformer";
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class Professional {
+  @IsNotEmpty()
+  @IsBoolean()
+  available: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  area: string;
+  
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  specialization: string;
+  
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  titleAbbreviation: string;
+  
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  email: string;
+  
+  @IsNotEmpty()
+  @IsNumber()
+  phone: number;
+}
