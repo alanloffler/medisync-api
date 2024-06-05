14cbbeeed87658407dfd0df5bafc83ab53e50968
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 19:53:22 -0300

    Changed validation for available (from boolean to number)

--- src/professionals/entities/professional.entity.ts
+++ src/professionals/entities/professional.entity.ts
@@ -1,26 +1,26 @@
-import { Transform } from "class-transformer";
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class Professional {
   @IsNotEmpty()
-  @IsBoolean()
-  available: boolean;
+  @IsNumber()
+  available: number;
 
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value.trim())
   area: string;
-  
+
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value.trim())
   specialization: string;
-  
+
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value.trim())
   titleAbbreviation: string;
-  
+
   @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value.trim())
@@ -35,7 +35,7 @@ export class Professional {
   @IsString()
   @Transform(({ value }) => value.trim())
   email: string;
-  
+
   @IsNotEmpty()
   @IsNumber()
   phone: number;
