c90c3f9ba6a7010443ee5eca3a7d13d7e74729f9
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 19:51:51 -0300

    Add expected response

--- src/professionals/professionals.controller.ts
+++ src/professionals/professionals.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProfessionalsService } from './professionals.service';
+
 import { CreateProfessionalDto } from './dto/create-professional.dto';
+import { IResponse } from '../common/interfaces/response.interface';
+import { Professional } from './schema/professional.schema';
+import { ProfessionalsService } from './professionals.service';
 import { UpdateProfessionalDto } from './dto/update-professional.dto';
 
 @Controller('professionals')
@@ -8,17 +11,17 @@ export class ProfessionalsController {
   constructor(private readonly professionalsService: ProfessionalsService) {}
 
   @Post()
-  create(@Body() createProfessionalDto: CreateProfessionalDto) {
+  create(@Body() createProfessionalDto: CreateProfessionalDto): Promise<IResponse> {
     return this.professionalsService.create(createProfessionalDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Professional[]> {
     return this.professionalsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Professional> {
     return this.professionalsService.findOne(id);
   }
 
