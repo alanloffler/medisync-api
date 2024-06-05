f1c0f688b8670a0b5d444b83b5d4166b692ed7ac
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 19:52:41 -0300

    Add HttpException for create, findAll and findOne

--- src/professionals/professionals.service.ts
+++ src/professionals/professionals.service.ts
@@ -1,27 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProfessionalDto } from './dto/create-professional.dto';
-import { UpdateProfessionalDto } from './dto/update-professional.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model, isValidObjectId } from 'mongoose';
+
+import { CreateProfessionalDto } from './dto/create-professional.dto';
 import { Professional } from './schema/professional.schema';
-import { Model } from 'mongoose';
+import { UpdateProfessionalDto } from './dto/update-professional.dto';
+import { PROF_CONFIG } from 'src/common/config/professionals.config';
 
 @Injectable()
 export class ProfessionalsService {
-  constructor(@InjectModel('Professional') private readonly professionalModel: Model<Professional>) {}
-  // @InjectModel(Cat.name) private catModel: Model<Cat>
+  constructor(@InjectModel(Professional.name) private readonly professionalModel: Model<Professional>) {}
 
   async create(createProfessionalDto: CreateProfessionalDto) {
-    return await this.professionalModel.create(createProfessionalDto);
-    
+    const professional = await this.professionalModel.create(createProfessionalDto);
+    if (!professional) throw new HttpException(PROF_CONFIG.errors.notCreated, HttpStatus.BAD_REQUEST);
+    return { statusCode: 200, message: PROF_CONFIG.success.created, data: professional };
   }
 
   async findAll() {
-    return await this.professionalModel.find().exec();
-    // return `This action returns all professionals`;
+    const professionals = await this.professionalModel.find();
+    if (professionals.length === 0) throw new HttpException(PROF_CONFIG.success.empty, HttpStatus.NOT_FOUND);
+    
+    return professionals;
   }
 
-  async findOne(id: string) {
-    return await this.professionalModel.findById(id).exec();
+  async findOne(id: string): Promise<Professional> {
+    const isValid = isValidObjectId(id);
+    if (!isValid) throw new HttpException(PROF_CONFIG.errors.notValid, HttpStatus.BAD_REQUEST);
+
+    const professional = await this.professionalModel.findById(id);
+    if (!professional) throw new HttpException(PROF_CONFIG.errors.notFound, HttpStatus.NOT_FOUND);
+
+    return professional;
   }
 
   async update(id: string, updateProfessionalDto: UpdateProfessionalDto) {
