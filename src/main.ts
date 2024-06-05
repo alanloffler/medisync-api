a7a30cf9a2c814555c661ccc1b14fbd7040a2135
alanloffler <alanmatiasloffler@gmail.com>
2024-06-05 19:48:19 -0300

    Add globalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT: number = parseInt(process.env.PORT) || 3000;
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT);
 }
