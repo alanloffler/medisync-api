615d83d64da9c42edd0b2d2b76936fe0b4441a2e
alanloffler <alanmatiasloffler@gmail.com>
2024-06-13 12:45:39 -0300

    Added setting for updated actions

--- src/common/config/professionals.config.ts
+++ src/common/config/professionals.config.ts
@@ -2,11 +2,13 @@ export const PROF_CONFIG = {
   success: {
     created: 'Profesional creado con éxito',
     empty: 'No hay profesionales en la base de datos',
-    searchNotFound: 'La búsqueda no ha arrojado resultados'
+    searchNotFound: 'La búsqueda no ha arrojado resultados',
+    updated: 'Profesional actualizado con éxito',
   },
   errors: {
     notCreated: 'El profesional no se pudo crear en la base de datos',
     notFound: 'Profesional no encontrado en la base de datos',
     notValid: 'ID inválido. La estructura del ID es incorrecta',
+    notUpdated: 'El profesional no se pudo actualizar en la base de datos',
   }
 }
