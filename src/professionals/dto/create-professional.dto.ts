d6d7df5f80ec2aba9125a1f86c79b590d221b13a
alanloffler <alanmatiasloffler@gmail.com>
2024-06-17 13:29:39 -0300

    New class for professionals configuration. Used on appo management. Class for validations on nested object.

--- src/professionals/dto/create-professional.dto.ts
+++ src/professionals/dto/create-professional.dto.ts
@@ -1,5 +1,27 @@
-import { Transform } from 'class-transformer';
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNotEmptyObject, IsNumber, IsObject, IsString, MinLength, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class Configuration {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  scheduleTimeInit: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  scheduleTimeEnd: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  timeSlotUnavailableInit: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  timeSlotUnavailableEnd: string;
+}
 
 export class CreateProfessionalDto {
   @IsNotEmpty()
@@ -8,35 +30,44 @@ export class CreateProfessionalDto {
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   area: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   specialization: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   titleAbbreviation: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   firstName: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   lastName: string;
 
   @IsNotEmpty()
   @IsString()
-  @Transform(({ value }) => value.trim())
   email: string;
 
   @IsNotEmpty()
   @IsNumber()
   phone: number;
+
+  @IsString()
+  scheduleTimeInit: string;
+
+  @IsString()
+  scheduleTimeEnd: string;
+
+  @IsString()
+  timeSlotUnavailableInit: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Configuration)
+  configuration: Configuration;
 }
