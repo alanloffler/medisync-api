623ec39cc687fc7727051d0ce6bc655b152203f6
alanloffler <alanmatiasloffler@gmail.com>
2024-06-17 13:28:06 -0300

    Added configuration prop for appointment management

--- src/professionals/schema/professional.schema.ts
+++ src/professionals/schema/professional.schema.ts
@@ -2,6 +2,7 @@ import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 import { Specialization } from '../../specializations/schema/specializations.schema';
+import { IConfiguration } from '../interfaces/configuration.interface';
 
 export type ProfessionalDocument = HydratedDocument<Professional>;
 
@@ -40,6 +41,22 @@ export class Professional {
 
   @Prop()
   phone: number;
+
+  @Prop({
+    type: {
+      scheduleTimeInit: { type: String, default: '08:00:00', trim: true },
+      scheduleTimeEnd: { type: String, default: '18:00:00', trim: true },
+      timeSlotUnavailableInit: { type: String, default: '12:00:00', trim: true },
+      timeSlotUnavailableEnd: { type: String, default: '14:00:00', trim: true },
+    },
+    default: {
+      scheduleTimeInit: '08:00:00',
+      scheduleTimeEnd: '18:00:00',
+      timeSlotUnavailableInit: '12:00:00',
+      timeSlotUnavailableEnd: '14:00:00',
+    },
+  })
+  configuration: IConfiguration;
 }
 
 export const ProfessionalSchema = SchemaFactory.createForClass(Professional);
