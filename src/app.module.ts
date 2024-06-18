87386e16c948387bf4196a40ce150346ac7cf903
alanloffler <alanmatiasloffler@gmail.com>
2024-06-18 15:43:09 -0300

    Adde Appointment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProfessionalsModule } from './professionals/professionals.module';
 import { AreasModule } from './areas/areas.module';
 import { SpecializationsModule } from './specializations/specializations.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { SpecializationsModule } from './specializations/specializations.module'
       autoCreate: false,
     }),
     ProfessionalsModule,
+    AppointmentsModule,
     AreasModule,
     SpecializationsModule,
   ],
