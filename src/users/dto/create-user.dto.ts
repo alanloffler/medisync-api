b39ac7fb023b7d014a6b38435137e8e6ba182712
alanloffler <alanmatiasloffler@gmail.com>
2024-06-22 12:21:31 -0300

    Created user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,27 @@
-import { IsEmail, IsInt, IsString, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsNumberString, IsString, MaxLength, MinLength } from "class-validator";
+import { USER_CONFIG } from "src/config/users.config";
 
 export class CreateUserDto {
-  @IsString({ message: 'First name must be a string' })
+  @IsString({ message: USER_CONFIG.validation.firstName.message })
+  @MinLength(3, { message: USER_CONFIG.validation.firstName.min })
+  @MaxLength(30, { message: USER_CONFIG.validation.firstName.max })
   firstName: string;
 
-  @IsString({ message: 'Last name must be a string' })
+  @IsString({ message: USER_CONFIG.validation.lastName.message })
+  @MinLength(3, { message: USER_CONFIG.validation.lastName.min })
+  @MaxLength(30, { message: USER_CONFIG.validation.lastName.max })
   lastName: string;
 
-  @IsInt({ message: 'DNI must be a number' })
-  @MinLength(7, { message: 'DNI must have at least 7 digits' })
-  @MaxLength(8, { message: 'DNI must have at most 8 digits' })
-  dni: number;
+  @IsNumberString({ no_symbols: true }, { message: USER_CONFIG.validation.dni.message })
+  @MinLength(7, { message: USER_CONFIG.validation.dni.min }) // 1 mill min
+  @MaxLength(8, { message: USER_CONFIG.validation.dni.max }) // 99.99 mill max
+  dni: string;
 
-  @IsInt({ message: 'Phone must be a number' })
-  phone: number;
+  @IsNumberString({ no_symbols: true }, { message: USER_CONFIG.validation.phone.message })
+  @MinLength(10, { message: USER_CONFIG.validation.phone.min }) // 0000 000000
+  @MaxLength(10, { message: USER_CONFIG.validation.phone.max }) // 0000 000000
+  phone: string;
   
-  @IsEmail()
+  @IsEmail({}, { message: USER_CONFIG.validation.email.message })
   email: string;
 }
