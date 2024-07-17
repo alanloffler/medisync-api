a1d488d81a7f4925fb3ffda5aea21cf66bbb0db5
alanloffler <alanmatiasloffler@gmail.com>
2024-07-17 20:12:12 -0300

    Dynamic validation messages for classes

--- src/professionals/dto/create-professional.dto.ts
+++ src/professionals/dto/create-professional.dto.ts
@@ -4,51 +4,50 @@ import { IWorkingDay } from '../interfaces/configuration.interface';
 import { PROFESSIONAL_CONFIG } from 'src/config/professional.config';
 
 class WorkingDayDto {
-  @IsInt({ message: PROFESSIONAL_CONFIG.validation.day.message })
-  @Min(0, { message: PROFESSIONAL_CONFIG.validation.day.min })
-  @Max(5, { message: PROFESSIONAL_CONFIG.validation.day.max })
+  @IsInt({ message: PROFESSIONAL_CONFIG.validation.workingDays.day.isInt })
+  @Min(0, { message: PROFESSIONAL_CONFIG.validation.workingDays.day.min })
+  @Max(5, { message: PROFESSIONAL_CONFIG.validation.workingDays.day.max })
   day: number;
 
-  @IsBoolean({ message: PROFESSIONAL_CONFIG.validation.value.message })
+  @IsBoolean({ message: PROFESSIONAL_CONFIG.validation.workingDays.value.isBoolean })
   value: boolean;
 }
 
 class Configuration {
-  @IsString({ message: 'La hora de inicio de agenda debe ser hh:mm' })
-  @IsNotEmpty({ message: 'La hora de inicio de agenda es obligatoria' })
-  @MinLength(5, { message: 'La hora de inicio de agenda debe poseer 5 caracteres' })
+  @IsString({ message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeInit.isString })
+  @IsNotEmpty({ message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeInit.isNotEmpty })
+  @MinLength(5, { message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeInit.minLength })
   scheduleTimeInit: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(5)
+  @IsString({ message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeEnd.isString })
+  @IsNotEmpty({ message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeEnd.isNotEmpty })
+  @MinLength(5, { message: PROFESSIONAL_CONFIG.validation.configuration.scheduleTimeEnd.minLength })
   scheduleTimeEnd: string;
 
-  @IsInt()
-  @IsPositive()
+  @IsInt({ message: PROFESSIONAL_CONFIG.validation.configuration.slotDuration.isInt })
+  @IsPositive({ message: PROFESSIONAL_CONFIG.validation.configuration.slotDuration.isPositive })
   slotDuration: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(5)
+  @IsString({ message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableInit.isString })
+  @IsNotEmpty({ message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableInit.isNotEmpty })
+  @MinLength(5, { message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableInit.minLength })
   timeSlotUnavailableInit: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(5)
+  @IsString({ message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableEnd.isString })
+  @IsNotEmpty({ message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableEnd.isNotEmpty })
+  @MinLength(5, { message: PROFESSIONAL_CONFIG.validation.configuration.timeSlotUnavailableEnd.minLength })
   timeSlotUnavailableEnd: string;
 
-  @ArrayNotEmpty({ message: PROFESSIONAL_CONFIG.validation.workingDays.notEmptyArray })
-  // @ValidateNested({ each: true })
+  @ArrayNotEmpty({ message: PROFESSIONAL_CONFIG.validation.workingDays.arrayNotEmpty })
+  @ValidateNested({ each: true })
   @ArrayMaxSize(6, { message: PROFESSIONAL_CONFIG.validation.workingDays.minLength })
-  // @Length(6, 6, { message: PROFESSIONAL_CONFIG.validation.workingDays.minLength})
   @Type(() => WorkingDayDto)
   workingDays: IWorkingDay[];
 }
 
 export class CreateProfessionalDto {
-  @IsNotEmpty()
-  @IsBoolean()
+  @IsNotEmpty({ message: PROFESSIONAL_CONFIG.validation.createProfessionalDto.available.isNotEmpty })
+  @IsBoolean({ message: PROFESSIONAL_CONFIG.validation.createProfessionalDto.available.isBoolean })
   available: boolean;
 
   @IsNotEmpty()
