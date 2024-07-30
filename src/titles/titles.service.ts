a561d552409c48c940085557da0e67bbd866aa7d
alanloffler <alanmatiasloffler@gmail.com>
2024-07-30 20:09:34 -0300

    findAll with sorting by name asc

--- src/titles/titles.service.ts
+++ src/titles/titles.service.ts
@@ -8,21 +8,25 @@ import { Model } from 'mongoose';
 @Injectable()
 export class TitlesService {
   constructor(@InjectModel(Title.name) private readonly titleModel: Model<Title>) {}
+
   async create(createTitleDto: CreateTitleDto) {
     const title = await this.titleModel.create(createTitleDto);
     if (!title) throw new HttpException('Title not created', HttpStatus.BAD_REQUEST);
+
     return { statusCode: 200, message: 'Title created', data: title };
   }
 
   async findAll() {
-    const titles = await this.titleModel.find();
+    const titles = await this.titleModel.find().sort({ name: 'asc' });
     if (!titles) throw new HttpException('Titles not found', HttpStatus.BAD_REQUEST);
+
     return { statusCode: 200, message: 'Titles found', data: titles };
   }
 
   async findOne(id: string) {
     const title = await this.titleModel.findById(id);
     if (!title) throw new HttpException('Title not found', HttpStatus.BAD_REQUEST);
+    
     return { statusCode: 200, message: 'Title found', data: title };
   }
 
