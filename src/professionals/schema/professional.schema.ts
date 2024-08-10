b4e267cf00124723632080c703697c7782e8987e
alanloffler <alanmatiasloffler@gmail.com>
2024-08-10 17:50:44 -0300

    Renamed title property

--- src/professionals/schema/professional.schema.ts
+++ src/professionals/schema/professional.schema.ts
@@ -64,7 +64,7 @@ export class Professional {
     required: true,
     type: MongooseSchema.Types.ObjectId,
   })
-  titleAbbreviation: string;
+  title: string;
 }
 
 export const ProfessionalSchema = SchemaFactory.createForClass(Professional);
