6279ad5d1bc60d686858b4e6613031ab0612f9fa
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 15:57:05 -0300

    Dynamic validation messages

--- src/config/specializations.config.ts
+++ src/config/specializations.config.ts
@@ -17,4 +17,24 @@ export const SPECIALIZATIONS_CONFIG = {
       updated: 'Especialidad actualizada con éxito',
     },
   },
+  validation: {
+    isIn: {
+      active: 'El estado debe ser 0 o 1',
+    },
+    isNotEmpty: {
+      area: 'El area es obligatoria',
+      description: 'La descripción es obligatoria',
+      name: 'El nombre es obligatorio',
+      plural: 'El plural es obligatorio',
+    },
+    isNumber: {
+      active: 'El estado debe ser un número',
+    },
+    isString: {
+      area: 'El area debe ser una cadena de texto',
+      description: 'La descripción debe ser una cadena de texto',
+      name: 'El nombre debe ser una cadena de texto',
+      plural: 'El plural debe ser una cadena de texto',
+    },
+  },
 };
--- src/specializations/dto/create-specialization.dto.ts
+++ src/specializations/dto/create-specialization.dto.ts
@@ -1,23 +1,24 @@
 import { IsIn, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { SPECIALIZATIONS_CONFIG as SPEC_CONFIG } from '@config/specializations.config';
 
 export class CreateSpecializationDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: SPEC_CONFIG.validation.isString.name })
+  @IsNotEmpty({ message: SPEC_CONFIG.validation.isNotEmpty.name })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: SPEC_CONFIG.validation.isString.plural })
+  @IsNotEmpty({ message: SPEC_CONFIG.validation.isNotEmpty.plural })
   plural: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: SPEC_CONFIG.validation.isString.description })
+  @IsNotEmpty({ message: SPEC_CONFIG.validation.isNotEmpty.description })
   description: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: SPEC_CONFIG.validation.isString.area })
+  @IsNotEmpty({ message: SPEC_CONFIG.validation.isNotEmpty.area })
   area: string;
 
-  @IsNumber()
-  @IsIn([0, 1])
+  @IsNumber({}, { message: SPEC_CONFIG.validation.isNumber.active })
+  @IsIn([0, 1], { message: SPEC_CONFIG.validation.isIn.active })
   active: number;
 }
