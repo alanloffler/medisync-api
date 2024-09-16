da84a0c8305d58dd52f14805942da19526a5693a
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 10:09:47 -0300

    Removed unused update method

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -30,11 +30,6 @@ export class AppointmentsController {
     return this.appointmentsService.findOne(id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAppointmentDto: UpdateAppointmentDto) {
-  //   return this.appointmentsService.update(+id, updateAppointmentDto);
-  // }
-
   @Delete(':id')
   remove(@Param('id') id: string): Promise<IResponse> {
     return this.appointmentsService.remove(id);
