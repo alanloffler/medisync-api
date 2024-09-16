3bf8a55de12fecd96319707f188b7ec7538219ad
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 14:12:01 -0300

    Renamed users config

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, isValidObjectId } from 'mongoose';
 import { CreateUserDto } from '@users/dto/create-user.dto';
 import { IResponse } from '@common/interfaces/response.interface';
-import { USER_CONFIG } from '@config/users.config';
+import { USERS_CONFIG } from '@config/users.config';
 import { UpdateUserDto } from '@users/dto/update-user.dto';
 import { User } from '@users/schema/user.schema';
 
@@ -13,12 +13,12 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto): Promise<IResponse> {
     const findUser = await this.findOneByDni(createUserDto.dni);
-    if (findUser) throw new HttpException(USER_CONFIG.service.error.userExistence, HttpStatus.BAD_REQUEST);
+    if (findUser) throw new HttpException(USERS_CONFIG.service.error.userExistence, HttpStatus.BAD_REQUEST);
 
     const user = await this.userModel.create(createUserDto);
-    if (!user) throw new HttpException(USER_CONFIG.service.error.userCreation, HttpStatus.BAD_REQUEST);
+    if (!user) throw new HttpException(USERS_CONFIG.service.error.userCreation, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userCreation, data: user };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userCreation, data: user };
   }
   // Find all users by firstName and lastName
   async findAll(search: string, limit: string, skip: string, sortingKey: string, sortingValue: string): Promise<IResponse> {
@@ -39,8 +39,8 @@ export class UsersService {
       .limit(parseInt(limit))
       .exec();
 
-    if (!users) throw new HttpException(USER_CONFIG.service.error.userFoundMany, HttpStatus.NOT_FOUND);
-    if (users.length === 0) throw new HttpException(USER_CONFIG.service.success.userFoundManyEmpty, HttpStatus.NOT_FOUND);
+    if (!users) throw new HttpException(USERS_CONFIG.service.error.userFoundMany, HttpStatus.NOT_FOUND);
+    if (users.length === 0) throw new HttpException(USERS_CONFIG.service.success.userFoundManyEmpty, HttpStatus.NOT_FOUND);
     // Data for pagination
     const count = await this.userModel
       .find({
@@ -55,7 +55,7 @@ export class UsersService {
     const pageTotal = Math.floor((count - 1) / parseInt(limit)) + 1;
     const data = { total: pageTotal, count: count, data: users };
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userFoundMany, data: data };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userFoundMany, data: data };
   }
   // Find all users by DNI (partial search of DNI => many results)
   async findAllByDNI(search: string, limit: string, skip: string, sortingKey: string, sortingValue: string): Promise<IResponse> {
@@ -77,8 +77,8 @@ export class UsersService {
       .limit(parseInt(limit))
       .exec();
 
-    if (!users) throw new HttpException(USER_CONFIG.service.error.userFoundMany, HttpStatus.NOT_FOUND);
-    if (users.length === 0) throw new HttpException(USER_CONFIG.service.success.userFoundManyEmpty, HttpStatus.NOT_FOUND);
+    if (!users) throw new HttpException(USERS_CONFIG.service.error.userFoundMany, HttpStatus.NOT_FOUND);
+    if (users.length === 0) throw new HttpException(USERS_CONFIG.service.success.userFoundManyEmpty, HttpStatus.NOT_FOUND);
     // Data for pagination
     const count = await this.userModel
       .find({
@@ -94,22 +94,22 @@ export class UsersService {
     const pageTotal = Math.floor((count - 1) / parseInt(limit)) + 1;
     const data = { total: pageTotal, count: count, data: users };
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userFoundMany, data: data };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userFoundMany, data: data };
   }
 
   async findOne(id: string): Promise<IResponse> {
     const isValid = isValidObjectId(id);
-    if (!isValid) throw new HttpException(USER_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
+    if (!isValid) throw new HttpException(USERS_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
 
     const user = await this.userModel.findById(id);
-    if (!user) throw new HttpException(USER_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
+    if (!user) throw new HttpException(USERS_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userFound, data: user };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userFound, data: user };
   }
 
   async findOneByDni(dni: number): Promise<IResponse> {
     const user = await this.userModel.findOne({ dni });
-    if (user) throw new HttpException(USER_CONFIG.service.error.userExistence, HttpStatus.NOT_FOUND);
+    if (user) throw new HttpException(USERS_CONFIG.service.error.userExistence, HttpStatus.NOT_FOUND);
 
     return;
   }
@@ -117,30 +117,30 @@ export class UsersService {
   async update(id: string, updateUserDto: UpdateUserDto): Promise<IResponse> {
     // throw new HttpException(USER_CONFIG.service.error.userUpdate, HttpStatus.BAD_REQUEST);
     const isValid = isValidObjectId(id);
-    if (!isValid) throw new HttpException(USER_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
+    if (!isValid) throw new HttpException(USERS_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
 
     const findDni = await this.userModel.findOne({ dni: updateUserDto.dni });
-    if (findDni && findDni._id.toString() !== id) throw new HttpException(USER_CONFIG.service.error.userExistence, HttpStatus.BAD_REQUEST);
+    if (findDni && findDni._id.toString() !== id) throw new HttpException(USERS_CONFIG.service.error.userExistence, HttpStatus.BAD_REQUEST);
 
     const findUser = await this.userModel.findById(id);
-    if (!findUser) throw new HttpException(USER_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
+    if (!findUser) throw new HttpException(USERS_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
 
     const user = await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
-    if (!user) throw new HttpException(USER_CONFIG.service.error.userUpdate, HttpStatus.BAD_REQUEST);
+    if (!user) throw new HttpException(USERS_CONFIG.service.error.userUpdate, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userUpdate };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userUpdate };
   }
 
   async remove(id: string): Promise<IResponse> {
     const isValid = isValidObjectId(id);
-    if (!isValid) throw new HttpException(USER_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
+    if (!isValid) throw new HttpException(USERS_CONFIG.service.error.userInvalidId, HttpStatus.BAD_REQUEST);
 
     const findUser = await this.userModel.findById(id);
-    if (!findUser) throw new HttpException(USER_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
+    if (!findUser) throw new HttpException(USERS_CONFIG.service.error.userFound, HttpStatus.NOT_FOUND);
 
     const user = await this.userModel.findByIdAndDelete(id);
-    if (!user) throw new HttpException(USER_CONFIG.service.error.userRemove, HttpStatus.BAD_REQUEST);
+    if (!user) throw new HttpException(USERS_CONFIG.service.error.userRemove, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: USER_CONFIG.service.success.userRemove, data: user };
+    return { statusCode: 200, message: USERS_CONFIG.service.success.userRemove, data: user };
   }
 }
