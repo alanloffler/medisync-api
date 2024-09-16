83d6b6ac1a3f8627c172037fa1593a5d24ed63e1
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 14:09:36 -0300

    ..

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,24 +14,12 @@ export class UsersController {
   }
 
   @Get()
-  findAll(
-    @Query('search') search: string, 
-    @Query('limit') limit: string, 
-    @Query('skip') skip: string, 
-    @Query('sk') sortingKey: string, 
-    @Query('sv') sortingValue: string
-  ): Promise<IResponse> {
+  findAll(@Query('search') search: string, @Query('limit') limit: string, @Query('skip') skip: string, @Query('sk') sortingKey: string, @Query('sv') sortingValue: string): Promise<IResponse> {
     return this.usersService.findAll(search, limit, skip, sortingKey, sortingValue);
   }
 
   @Get('byDNI')
-  findAllByDNI(
-    @Query('search') search: string, 
-    @Query('limit') limit: string, 
-    @Query('skip') skip: string, 
-    @Query('sk') sortingKey: string, 
-    @Query('sv') sortingValue: string
-  ): Promise<IResponse> {
+  findAllByDNI(@Query('search') search: string, @Query('limit') limit: string, @Query('skip') skip: string, @Query('sk') sortingKey: string, @Query('sv') sortingValue: string): Promise<IResponse> {
     return this.usersService.findAllByDNI(search, limit, skip, sortingKey, sortingValue);
   }
 
