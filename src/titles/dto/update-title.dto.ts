3d136526d2188d812c7af1f3c109b3a4a164cdc7
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 15:00:01 -0300

    Alias for import path

--- src/titles/dto/update-title.dto.ts
+++ src/titles/dto/update-title.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateTitleDto } from './create-title.dto';
+import { CreateTitleDto } from '@titles/dto/create-title.dto';
 
 export class UpdateTitleDto extends PartialType(CreateTitleDto) {}
