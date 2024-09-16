8ff70b16c7288c4ae8b608aab43f2fa1ad389501
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 12:26:40 -0300

    Dynamic responses messages

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -12,17 +12,17 @@ export class AppointmentsService {
 
   async create(createAppointmentDto: CreateAppointmentDto): Promise<IResponse> {
     const appointment = await this.appointmentModel.create(createAppointmentDto);
-    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.notCreated, HttpStatus.BAD_REQUEST);
+    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.error.notCreated, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.created, data: appointment };
+    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.created, data: appointment };
   }
 
   async findAll(): Promise<IResponse> {
     const appointments = await this.appointmentModel.find();
-    if (!appointments) throw new HttpException(APPOINTMENTS_CONFIG.response.notFoundPlural, HttpStatus.NOT_FOUND);
-    if (appointments.length === 0) throw new HttpException(APPOINTMENTS_CONFIG.response.notFoundPlural, HttpStatus.NOT_FOUND);
+    if (!appointments) throw new HttpException(APPOINTMENTS_CONFIG.response.error.notFoundPlural, HttpStatus.NOT_FOUND);
+    if (appointments.length === 0) throw new HttpException(APPOINTMENTS_CONFIG.response.error.notFoundPlural, HttpStatus.NOT_FOUND);
 
-    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.foundPlural, data: appointments };
+    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.foundPlural, data: appointments };
   }
 
   async findAllByProfessional(id: string, day: string): Promise<IResponse> {
@@ -30,9 +30,9 @@ export class AppointmentsService {
       .find({ professional: id, day: day })
       .populate({ path: 'user', select: '_id firstName lastName dni' });
 
-    if (!appointments || appointments.length === 0) return { statusCode: 404, message: APPOINTMENTS_CONFIG.response.notFoundPlural, data: [] };
+    if (!appointments || appointments.length === 0) return { statusCode: 404, message: APPOINTMENTS_CONFIG.response.error.notFoundPlural, data: [] };
 
-    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.foundPlural, data: appointments };
+    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.foundPlural, data: appointments };
   }
 
   async findOne(id: string): Promise<IResponse> {
@@ -41,15 +41,15 @@ export class AppointmentsService {
       .populate({ path: 'professional', select: '_id firstName lastName', populate: { path: 'title', select: 'abbreviation' } })
       .populate({ path: 'user', select: '_id firstName lastName dni' });
 
-    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.notFoundSingular, HttpStatus.NOT_FOUND);
+    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.error.notFoundSingular, HttpStatus.NOT_FOUND);
 
-    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.foundSingular, data: appointment };
+    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.foundSingular, data: appointment };
   }
 
   async remove(id: string): Promise<IResponse> {
     const appointment = await this.appointmentModel.findByIdAndDelete(id);
-    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.notRemoved, HttpStatus.NOT_FOUND);
+    if (!appointment) throw new HttpException(APPOINTMENTS_CONFIG.response.error.notRemoved, HttpStatus.NOT_FOUND);
 
-    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.removed, data: appointment };
+    return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.removed, data: appointment };
   }
 }
