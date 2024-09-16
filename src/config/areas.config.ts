2bf368fbda0452bfaedd0348ea89cff53b28ffbd
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 12:27:37 -0300

    Responses messages

--- src/config/areas.config.ts
+++ src/config/areas.config.ts
@@ -1,4 +1,25 @@
-export const AREA_CONFIG = {
+export const AREAS_CONFIG = {
+  response: {
+    error: {
+      errorDuplicate: 'Área ya existente',
+      errorFinding: 'Error al buscar el área',
+      errorFindingMany: 'Error al buscar las áreas',
+      notCreated: 'Área no creada',
+      notDeleted: 'Área no eliminada',
+      notFound: 'Área no encontrada',
+      notUpdated: 'Área no actualizada',
+      notValid: 'ID inválido. La estructura del ID es incorrecta',
+    },
+    success: {
+      created: 'Área creada',
+      deleted: 'Área eliminada',
+      empty: 'Área no existente',
+      emptyMany: 'No existen áreas',
+      found: 'Área encontrada',
+      foundMany: 'Áreas encontradas',
+      updated: 'Área actualizada',
+    },
+  },
   validation: {
     isIn: {
       active: 'El estado debe ser 0 o 1',
@@ -18,3 +39,7 @@ export const AREA_CONFIG = {
     },
   },
 };
+
+errors: {
+
+}
