31bbc3414e9cd08fd78aca063036d86656b53137
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 10:08:18 -0300

    Validation messages

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,22 +1,23 @@
+import { APPOINTMENTS_CONFIG } from '@config/appointments.config';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateAppointmentDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.professional })
+  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.professional })
   professional: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.user })
+  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.user })
   user: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.day })
+  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.day })
   day: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.hour })
+  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.hour })
   hour: string;
 
-  @IsInt()
+  @IsInt({ message: APPOINTMENTS_CONFIG.validation.isInt.slot })
   slot: number;
 }
--- src/config/appointments.config.ts
+++ src/config/appointments.config.ts
@@ -8,5 +8,22 @@ export const APPOINTMENTS_CONFIG = {
     notFoundPlural: 'Turnos no encontrados',
     notFoundSingular: 'Turno no encontrado',
     notRemoved: 'Turno no eliminado',
+  },
+  validation: {
+    isInt: {
+      slot: 'El turno debe ser un número entero',
+    },
+    isString: {
+      day: 'El día debe ser una cadena de texto',
+      hour: 'La hora debe ser una cadena de texto',
+      professional: 'El ID del profesional debe ser una cadena de texto',
+      user: 'El ID del usuario debe ser una cadena de texto',
+    },
+    isNotEmpty: {
+      day: 'El día es obligatorio',
+      hour: 'La hora es obligatoria',
+      professional: 'El ID del profesional es obligatorio',
+      user: 'El ID del usuario es obligatorio',
+    }
   }
 };
