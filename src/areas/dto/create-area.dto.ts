afc5fcc286c94d1784eadb396c85b123c4fc0455
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 12:27:08 -0300

    Dynamic validation messages

--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,20 +1,20 @@
-import { AREA_CONFIG } from '@config/areas.config';
+import { AREAS_CONFIG } from '@config/areas.config';
 import { IsIn, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateAreaDto {
-  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.name })
-  @IsString({ message: AREA_CONFIG.validation.isString.name })
+  @IsNotEmpty({ message: AREAS_CONFIG.validation.isNotEmpty.name })
+  @IsString({ message: AREAS_CONFIG.validation.isString.name })
   name: string;
 
-  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.plural })
-  @IsString({ message: AREA_CONFIG.validation.isString.plural })
+  @IsNotEmpty({ message: AREAS_CONFIG.validation.isNotEmpty.plural })
+  @IsString({ message: AREAS_CONFIG.validation.isString.plural })
   plural: string;
 
-  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.description })
-  @IsString({ message: AREA_CONFIG.validation.isString.description })
+  @IsNotEmpty({ message: AREAS_CONFIG.validation.isNotEmpty.description })
+  @IsString({ message: AREAS_CONFIG.validation.isString.description })
   description: string;
 
-  @IsNumber({}, { message: AREA_CONFIG.validation.isNumber.active })
-  @IsIn([0, 1], { message: AREA_CONFIG.validation.isIn.active })
+  @IsNumber({}, { message: AREAS_CONFIG.validation.isNumber.active })
+  @IsIn([0, 1], { message: AREAS_CONFIG.validation.isIn.active })
   active: number;
 }
