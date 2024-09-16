26b5121a8b76b4f375c7e61f6bf83ee360b65fc0
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 15:31:36 -0300

    Alias for import path

--- src/specializations/dto/update-specialization.dto.ts
+++ src/specializations/dto/update-specialization.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateSpecializationDto } from './create-specialization.dto';
+import { CreateSpecializationDto } from '@specializations/dto/create-specialization.dto';
 
 export class UpdateSpecializationDto extends PartialType(CreateSpecializationDto) {}
