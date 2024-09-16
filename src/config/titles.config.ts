a69144bb73065ba3ea31df4582a20fa3acf252fd
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 15:04:12 -0300

    Dynamic validation messages

---
+++ src/config/titles.config.ts
@@ -0,0 +1,12 @@
+export const TITLES_CONFIG = {
+  validation: {
+    isNotEmpty: {
+      abbreviation: 'La abreviación es obligatoria',
+      name: 'El nombre es obligatorio',
+    },
+    isString: {
+      abbreviation: 'La abreviación debe ser una cadena de texto',
+      name: 'El nombre debe ser una cadena de texto',
+    },
+  },
+};
--- src/titles/dto/create-title.dto.ts
+++ src/titles/dto/create-title.dto.ts
@@ -1,11 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { TITLES_CONFIG } from '@/config/titles.config';
 
 export class CreateTitleDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: TITLES_CONFIG.validation.isString.name })
+  @IsNotEmpty({ message: TITLES_CONFIG.validation.isNotEmpty.name })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: TITLES_CONFIG.validation.isString.abbreviation })
+  @IsNotEmpty({ message: TITLES_CONFIG.validation.isNotEmpty.abbreviation })
   abbreviation: string;
 }
