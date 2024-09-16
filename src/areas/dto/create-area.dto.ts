25d7785c0d81c5262b9f40c3f1f11cb7dd3d94db
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 10:30:51 -0300

    Dynamic validation messages

--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,19 +1,20 @@
-import { IsIn, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { AREA_CONFIG } from '@config/areas.config';
+import { IsIn, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateAreaDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.name })
+  @IsString({ message: AREA_CONFIG.validation.isString.name })
   name: string;
-  
-  @IsString()
-  @IsNotEmpty()
+
+  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.plural })
+  @IsString({ message: AREA_CONFIG.validation.isString.plural })
   plural: string;
-  
-  @IsString()
-  @IsNotEmpty()
+
+  @IsNotEmpty({ message: AREA_CONFIG.validation.isNotEmpty.description })
+  @IsString({ message: AREA_CONFIG.validation.isString.description })
   description: string;
 
-  @IsNumber()
-  @IsIn([0, 1])
+  @IsNumber({}, { message: AREA_CONFIG.validation.isNumber.active })
+  @IsIn([0, 1], { message: AREA_CONFIG.validation.isIn.active })
   active: number;
 }
