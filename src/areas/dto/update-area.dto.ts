75d040978f8d2b894ede0f29ec7bee593f09937c
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 12:27:23 -0300

    Alias for import path

--- src/areas/dto/update-area.dto.ts
+++ src/areas/dto/update-area.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAreaDto } from './create-area.dto';
+import { CreateAreaDto } from '@areas/dto/create-area.dto';
 
 export class UpdateAreaDto extends PartialType(CreateAreaDto) {}
