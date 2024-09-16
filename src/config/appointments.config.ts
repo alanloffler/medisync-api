390d311379195c123836c4a19b1ff5a157d87216
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 09:57:01 -0300

    Appointment config file

---
+++ src/config/appointments.config.ts
@@ -0,0 +1,12 @@
+export const APPOINTMENTS_CONFIG = {
+  response: {
+    created: 'Turno creado',
+    foundPlural: 'Turnos encontrados',
+    foundSingular: 'Turno encontrado',
+    removed: 'Turno eliminado',
+    notCreated: 'Turno no creado',
+    notFoundPlural: 'Turnos no encontrados',
+    notFoundSingular: 'Turno no encontrado',
+    notRemoved: 'Turno no eliminado',
+  }
+};
