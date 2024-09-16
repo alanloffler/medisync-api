40cad8d8e39d11fa37f8278ac09ad98153eb5320
alanloffler <alanmatiasloffler@gmail.com>
2024-09-16 16:17:08 -0300

    Relocated messages for professionals responses

--- src/config/professionals.config.ts
+++ src/config/professionals.config.ts
@@ -1,4 +1,21 @@
 export const PROFESSIONALS_CONFIG = {
+  success: {
+    created: 'Profesional creado con éxito',
+    empty: 'No hay profesionales en la base de datos',
+    found: 'Profesional encontrado en la base de datos',
+    foundMany: 'Profesionales encontrados en la base de datos',
+    removed: 'Profesional eliminado con éxito',
+    searchNotFound: 'La búsqueda no ha arrojado resultados',
+    updated: 'Profesional actualizado con éxito',
+  },
+  errors: {
+    notCreated: 'El profesional no se pudo crear en la base de datos',
+    notFound: 'Profesionales no encontrados en la base de datos',
+    notFoundOne: 'Profesional no encontrado en la base de datos',
+    notRemoved: 'El profesional no se pudo eliminar en la base de datos',
+    notUpdated: 'El profesional no se pudo actualizar en la base de datos',
+    notValid: 'ID inválido. La estructura del ID es incorrecta',
+  },
   validation: {
     configuration: {
       scheduleTimeInit: {
