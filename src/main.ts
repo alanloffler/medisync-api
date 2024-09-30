c162b0dd2f34a4dd8768c05a585e0074fe08921a
alanloffler <alanmatiasloffler@gmail.com>
2024-09-30 13:13:59 -0300

    Typed app constant and order

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '@/app.module';
+import { NestFactory } from '@nestjs/core';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { formatValidationError } from '@common/validators/format-error';
 
 async function bootstrap() {
   const PORT: number = parseInt(process.env.PORT) || 3000;
 
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: process.env.FRONTEND_URL,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: formatValidationError,
@@ -18,4 +20,5 @@ async function bootstrap() {
 
   await app.listen(PORT);
 }
+
 bootstrap();
