5367430c4682428aa8508504a366f8c619e4241d
alanloffler <alanmatiasloffler@gmail.com>
2024-10-22 18:19:40 -0300

    Populated result with professional and user data

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -34,7 +34,10 @@ export class AppointmentsService {
   }
 
   async findAllByUser(id: string): Promise<IResponse> {
-    const appointments = await this.appointmentModel.find({ user: id });
+    const appointments = await this.appointmentModel
+    .find({ user: id })
+    .populate({ path: 'professional', select: '_id firstName lastName', populate: { path: 'title', select: 'abbreviation' } })
+    .populate({ path: 'user', select: '_id firstName lastName dni' });
 
     if (!appointments) return { statusCode: 404, message: APPOINTMENTS_CONFIG.response.error.notFoundPlural, data: [] };
     if (appointments.length === 0) return { statusCode: 200, message: APPOINTMENTS_CONFIG.response.success.empty, data: [] };
