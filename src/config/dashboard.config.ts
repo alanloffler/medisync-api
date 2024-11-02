622d1908c03b2c62e612bf917504fcbf00f5a774
alanloffler <alanmatiasloffler@gmail.com>
2024-11-02 18:07:33 -0300

    Dashboard config file

---
+++ src/config/dashboard.config.ts
@@ -0,0 +1,14 @@
+export const DASHBOARD_CONFIG = {
+  response: {
+    error: {
+      appointment: {
+        count: 'Error al contar el total de los turnos',
+      },
+    },
+    success: {
+      appointment: {
+        count: 'Total de turnos contados',
+      },
+    },
+  },
+};
