40d4847b1fc7da37a4cbe1be5d020432f7ab768e
alanloffler <alanmatiasloffler@gmail.com>
2024-11-02 19:45:52 -0300

    User response messages

--- src/config/dashboard.config.ts
+++ src/config/dashboard.config.ts
@@ -4,10 +4,16 @@ export const DASHBOARD_CONFIG = {
       appointment: {
         count: 'Error al contar el total de los turnos',
       },
+      user: {
+        count: 'Error al contar el total de los pacientes',
+      },
     },
     success: {
       appointment: {
         count: 'Total de turnos contados',
+      },
+      user: {
+        count: 'Total de pacientes contados',
       },
     },
   },
