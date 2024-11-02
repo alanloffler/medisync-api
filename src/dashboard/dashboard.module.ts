11c144338fbcd8aa9068c0dff238eb5ff118c601
alanloffler <alanmatiasloffler@gmail.com>
2024-11-02 19:49:10 -0300

    UsersModule injection

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppointmentsModule } from '@appointments/appointments.module';
 import { DashboardController } from '@dashboard/dashboard.controller';
 import { DashboardService } from '@dashboard/dashboard.service';
+import { UsersModule } from '@users/users.module';
 
 @Module({
-  imports: [AppointmentsModule],
+  imports: [AppointmentsModule, UsersModule],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
