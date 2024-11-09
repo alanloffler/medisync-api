ec89bf2076a87f2958cd36c38a09e0432b0fade5
alanloffler <alanmatiasloffler@gmail.com>
2024-11-09 19:04:11 -0300

    Professionals module import

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppointmentsModule } from '@appointments/appointments.module';
 import { DashboardController } from '@dashboard/dashboard.controller';
 import { DashboardService } from '@dashboard/dashboard.service';
+import { ProfessionalsModule } from '@/professionals/professionals.module';
 import { UsersModule } from '@users/users.module';
 
 @Module({
-  imports: [AppointmentsModule, UsersModule],
+  imports: [AppointmentsModule, ProfessionalsModule, UsersModule],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
