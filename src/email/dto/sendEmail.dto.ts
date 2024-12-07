da8264be56b0d0682a52de27be5e445f6e51fc57
alanloffler <alanmatiasloffler@gmail.com>
2024-12-07 20:33:33 -0300

    Fix: name of data received. Remove unused text property

--- src/email/dto/sendEmail.dto.ts
+++ src/email/dto/sendEmail.dto.ts
@@ -1,17 +1,13 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 import { EMAIL_CONFIG } from '@config/email.config';
 
 export class sendEmailDto {
-  @IsEmail({}, { message: EMAIL_CONFIG.validation.isEmail.recipients, each: true })
-  recipients: string[];
+  @IsEmail({}, { message: EMAIL_CONFIG.validation.isEmail.to, each: true })
+  to: string[];
 
   @IsString({ message: EMAIL_CONFIG.validation.isString.subject })
   subject: string;
 
-  @IsString({ message: EMAIL_CONFIG.validation.isString.html })
-  html: string;
-
-  @IsOptional({ message: EMAIL_CONFIG.validation.isString.text })
-  @IsString()
-  text?: string;
+  @IsString({ message: EMAIL_CONFIG.validation.isString.body })
+  body: string;
 }
