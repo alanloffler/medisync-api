de4bcc9d12a564e9c4175c79baa711121e4f09ac
alanloffler <alanmatiasloffler@gmail.com>
2024-12-10 23:35:07 -0300

    Add: UsersModule imported

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Appointment, AppointmentSchema } from '@appointments/schema/appointment.schema';
 import { AppointmentsController } from '@appointments/appointments.controller';
 import { AppointmentsService } from '@appointments/appointments.service';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Appointment.name, schema: AppointmentSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Appointment.name, schema: AppointmentSchema }]), UsersModule],
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
   exports: [MongooseModule],
