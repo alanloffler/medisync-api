d7fd3b03e79800d474d05a03dc9a82bbd3649349
alanloffler <alanmatiasloffler@gmail.com>
2025-01-21 12:08:19 -0300

    Update: app file size limit set to 10 mb

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { AppModule } from '@/app.module';
-import { NestFactory } from '@nestjs/core';
-import { json, raw, urlencoded } from 'express';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { formatValidationError } from '@common/validators/format-error';
+import { json, raw, urlencoded } from 'express';
 
 async function bootstrap() {
   const PORT: number = parseInt(process.env.PORT) || 3000;
@@ -19,9 +19,9 @@ async function bootstrap() {
     }),
   );
 
-  app.use(json({ limit: '15mb' }));
-  app.use(raw({ limit: '15mb' }));
-  app.use(urlencoded({ limit: '15mb', extended: true }));
+  app.use(json({ limit: '10mb' }));
+  app.use(raw({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
 
   await app.listen(PORT);
 }
