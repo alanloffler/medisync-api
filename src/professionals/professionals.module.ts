26ca2ccaec9d5c68562fe78dca29cd65962600b3
alanloffler <alanmatiasloffler@gmail.com>
2025-02-26 19:08:57 -0300

    Add: import AppointmentsModule

--- src/professionals/professionals.module.ts
+++ src/professionals/professionals.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppointmentsModule } from '@appointments/appointments.module';
 import { Professional, ProfessionalSchema } from '@professionals/schema/professional.schema';
 import { ProfessionalsController } from '@professionals/professionals.controller';
 import { ProfessionalsService } from '@professionals/professionals.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Professional.name, schema: ProfessionalSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Professional.name, schema: ProfessionalSchema }]), AppointmentsModule],
   controllers: [ProfessionalsController],
   providers: [ProfessionalsService],
   exports: [MongooseModule],
