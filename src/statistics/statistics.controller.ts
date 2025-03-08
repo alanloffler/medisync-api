d8a7784cd252741929ad7ff9df18a63f78009581
alanloffler <alanmatiasloffler@gmail.com>
2025-03-08 10:16:52 -0300

    Add professional ID parameter to appointments count

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import type { IResponse } from '@common/interfaces/response.interface';
 import { StatisticsService } from './statistics.service';
 
@@ -7,8 +7,8 @@ export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
   @Get('countApposByProfessional')
-  countApposByProfessional(): Promise<IResponse<number>> {
-    return this.statisticsService.countApposByProfessional();
+  countApposByProfessional(@Query('id') id: string): Promise<IResponse<number>> {
+    return this.statisticsService.countApposByProfessional(id);
   }
 
   @Get(':id')
