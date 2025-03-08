60a4e006ac663eedfb2bc63df4b735e5c9de83ee
alanloffler <alanmatiasloffler@gmail.com>
2025-03-08 10:43:49 -0300

    Update statistics endpoint return type to IStats

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import type { IResponse } from '@common/interfaces/response.interface';
-import { StatisticsService } from './statistics.service';
+import type { IStats } from '@statistics/interfaces/statistics.interface';
+import { StatisticsService } from '@statistics/statistics.service';
 
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
   @Get('countApposByProfessional')
-  countApposByProfessional(@Query('id') id: string): Promise<IResponse<number>> {
+  countApposByProfessional(@Query('id') id: string): Promise<IResponse<IStats>> {
     return this.statisticsService.countApposByProfessional(id);
   }
 
