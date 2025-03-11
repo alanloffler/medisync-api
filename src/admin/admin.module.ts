549fe506077c123e4307cc5f91d2d2c5034443ea
alanloffler <alanmatiasloffler@gmail.com>
2025-03-11 17:39:39 -0300

    Add Mongoose schema for Admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { AdminController } from './admin.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Admin, AdminSchema } from '@admin/schema/admin.schema';
+import { AdminController } from '@admin/admin.controller';
+import { AdminService } from '@admin/admin.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
   controllers: [AdminController],
   providers: [AdminService],
+  exports: [MongooseModule],
 })
 export class AdminModule {}
