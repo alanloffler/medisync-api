90691cf28c38f8dc9881145606e051309339fee3
alanloffler <alanmatiasloffler@gmail.com>
2025-03-11 16:56:14 -0300

    Add validation to CreateAdminDto for firstName, lastName and email properties

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAdminDto {}
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateAdminDto {
+  @IsString({ message: 'First name must be a string' })
+  @MinLength(3, { message: 'First name must be at least 3 characters long' })
+  @MaxLength(30, { message: 'First name must be at most 30 characters long' })
+  firstName: string;
+
+  @IsString({ message: 'Last name must be a string' })
+  @MinLength(3, { message: 'Last name must be at least 3 characters long' })
+  @MaxLength(30, { message: 'Last name must be at most 30 characters long' })
+  lastName: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Invalid email address' })
+  email: string;
+}
