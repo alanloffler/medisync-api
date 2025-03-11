0611f943feca0b3120cc31ddf05837540f2e6a90
alanloffler <alanmatiasloffler@gmail.com>
2025-03-11 17:38:16 -0300

    Create admin.schema.ts

---
+++ src/admin/schema/admin.schema.ts
@@ -0,0 +1,18 @@
+import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type AdminDocument = HydratedDocument<Admin>;
+
+@Schema({ timestamps: true })
+export class Admin {
+  @Prop({ lowercase: true, trim: true, required: true })
+  firstName: string;
+
+  @Prop({ lowercase: true, trim: true, required: true })
+  lastName: string;
+
+  @Prop({ lowercase: true, trim: true, unique: true, required: true })
+  email: string;
+}
+
+export const AdminSchema = SchemaFactory.createForClass(Admin);
