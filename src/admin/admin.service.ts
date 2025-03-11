458ca4e43f8f5d49902f950163275c85b7de36d8
alanloffler <alanmatiasloffler@gmail.com>
2025-03-11 17:39:04 -0300

    Implement Admin creation with MongoDB integration

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import type { IResponse } from '@common/interfaces/response.interface';
+import { Admin } from '@admin/schema/admin.schema';
 import { CreateAdminDto } from '@admin/dto/create-admin.dto';
 import { UpdateAdminDto } from '@admin/dto/update-admin.dto';
 
 @Injectable()
 export class AdminService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+  constructor(@InjectModel(Admin.name) private readonly adminModel: Model<Admin>) {}
+
+  async create(createAdminDto: CreateAdminDto): Promise<IResponse<Admin>> {
+    const admin = await this.adminModel.create(createAdminDto);
+    if (!admin) throw new HttpException('Failed to create admin', HttpStatus.BAD_REQUEST);
+
+    return { data: admin, message: 'Admin created successfully', statusCode: HttpStatus.CREATED };
   }
 
   findAll() {
