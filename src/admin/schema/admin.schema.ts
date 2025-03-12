71652e0260d4241f5e2fcf7758b71449fd66346e
alanloffler <alanmatiasloffler@gmail.com>
2025-03-11 22:20:56 -0300

    Add _id field with auto-generation to Admin schema

--- src/admin/schema/admin.schema.ts
+++ src/admin/schema/admin.schema.ts
@@ -1,10 +1,16 @@
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type AdminDocument = HydratedDocument<Admin>;
 
 @Schema({ timestamps: true })
 export class Admin {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    auto: true,
+  })
+  _id: string;
+
   @Prop({ lowercase: true, trim: true, required: true })
   firstName: string;
 
