de6271545470a43ada87f9932db5dff542e22ef5
alanloffler <alanmatiasloffler@gmail.com>
2025-03-12 09:53:48 -0300

    Add JWT authentication to admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,3 +1,4 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from '@admin/schema/admin.schema';
@@ -5,7 +6,13 @@ import { AdminController } from '@admin/admin.controller';
 import { AdminService } from '@admin/admin.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [MongooseModule],
