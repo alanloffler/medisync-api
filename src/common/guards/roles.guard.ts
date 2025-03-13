9871f702f9c57dff627f20394155bfc9a1e63436
alanloffler <alanmatiasloffler@gmail.com>
2025-03-13 17:47:57 -0300

    Update RolesGuard to support multiple roles (ERole[])

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,23 +1,23 @@
 import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { ERole } from '@common/enums/role.enum';
 import { ROLES_KEY } from '@common/decorators/roles.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const role: ERole = this.reflector.getAllAndOverride<ERole>(ROLES_KEY, [context.getHandler(), context.getClass()]);
-    if (!role) return true;
+    const roles: ERole[] = this.reflector.getAllAndOverride<ERole[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
+    if (!roles || roles.length === 0) return true;
 
     const { user } = context.switchToHttp().getRequest();
-    if (user.role === ERole.Admin) return true;
+    const roleExists = roles.some((role) => user.role?.includes(role));
 
-    if (role === user.role) {
-      return true;
-    } else {
+    if (!roleExists) {
       throw new HttpException('Credentials are not valid', HttpStatus.FORBIDDEN);
+    } else {
+      return true;
     }
   }
 }
