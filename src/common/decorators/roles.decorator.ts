ec1f976ef20522d61aeb7e1290ea5f63bf92cc97
alanloffler <alanmatiasloffler@gmail.com>
2025-03-13 17:55:15 -0300

    Update Roles decorator to support multiple roles (ERole[]) or to be empty

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,6 +1,6 @@
-import { SetMetadata, type CustomDecorator } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
 import { ERole } from '@common/enums/role.enum';
 
 export const ROLES_KEY: string = 'roles';
 
-export const Roles: (role: ERole) => CustomDecorator<string> = (role: ERole) => SetMetadata(ROLES_KEY, [role]);
+export const Roles = (roles: ERole[] | null = []) => SetMetadata(ROLES_KEY, roles);
