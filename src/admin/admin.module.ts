2e9f0c3254b07e22f89eee372d2b59ea3a872c76
alanloffler <alanmatiasloffler@gmail.com>
2025-03-13 09:18:11 -0300

    Remove import of JwtModule and add provider JwtService in admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,5 +1,4 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from '@admin/schema/admin.schema';
@@ -7,19 +6,9 @@ import { AdminController } from '@admin/admin.controller';
 import { AdminService } from '@admin/admin.service';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        global: true,
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
-      }),
-    }),
-    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, JwtService],
   exports: [MongooseModule],
 })
 export class AdminModule {}
