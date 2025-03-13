6765e806754884a4e171229e6f9004661ad9412a
alanloffler <alanmatiasloffler@gmail.com>
2025-03-13 17:43:30 -0300

    Create auth decorator

---
+++ src/common/decorators/auth.decorator.ts
@@ -0,0 +1,9 @@
+import { UseGuards, applyDecorators } from '@nestjs/common';
+import { AuthGuard } from '@common/guards/auth.guard';
+import { ERole } from '@common/enums/role.enum';
+import { Roles } from '@common/decorators/roles.decorator';
+import { RolesGuard } from '@common/guards/roles.guard';
+
+export function Auth(roles: ERole[]) {
+  return applyDecorators(Roles(roles), UseGuards(AuthGuard, RolesGuard));
+}
