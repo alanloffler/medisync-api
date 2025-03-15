d4204435bfeebde4d3fb403bd9dd13a9ffde3f92
alanloffler <alanmatiasloffler@gmail.com>
2025-03-14 21:10:04 -0300

    Add auth dependencies to AdminModule providers

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,11 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from '@admin/schema/admin.schema';
 import { AdminController } from '@admin/admin.controller';
 import { AdminService } from '@admin/admin.service';
+import { AuthService } from '@auth/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, AuthService, ConfigService, JwtService],
   exports: [MongooseModule],
 })
 export class AdminModule {}
