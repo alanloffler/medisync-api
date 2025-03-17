80ca3f21b4ebe3a71b5bf795d9ff6f247b5beaa4
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 17:17:55 -0300

    Relocate ITokens interface and remove from IPayload interface

--- src/auth/interface/payload.interface.ts
+++ src/auth/interface/payload.interface.ts
@@ -2,10 +2,4 @@ export interface IPayload {
   _id: string;
   email: string;
   role: string;
-  tokens: ITokens;
-}
-
-export interface ITokens {
-  accessToken: string;
-  refreshToken: string;
 }
