2b9d1ad557932a833c5ea8aea8a96f0156abd046
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 17:19:12 -0300

    Add token parameter to refreshTokens controller method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import type { IPayload } from '@auth/interface/payload.interface';
 import type { IRequest } from '@auth/interface/request.interface';
 import type { IResponse } from '@common/interfaces/response.interface';
+import type { ITokens } from '@auth/interface/tokens.interface';
 import { Auth } from '@common/decorators/auth.decorator';
 import { AuthService } from '@auth/auth.service';
 import { ERole } from '@common/enums/role.enum';
@@ -24,7 +25,7 @@ export class AuthController {
 
   @Auth([ERole.Admin, ERole.Super])
   @Get('refreshTokens')
-  refreshTokens(@Req() req: IRequest): Promise<IResponse<IPayload>> {
-    return this.authService.refreshTokens(req.user);
+  refreshTokens(@Req() req: IRequest, @Body() tokens: ITokens): Promise<IResponse<IPayload>> {
+    return this.authService.refreshTokens(req.user, tokens);
   }
 }
