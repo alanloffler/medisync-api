5bd2e41fd4e53e2315e69170048def6f24ac0854
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 11:15:49 -0300

    Add logout endpoint with role-based authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import type { ILogin } from '@auth/interface/login.interface';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import type { IPayload } from '@auth/interface/payload.interface';
+import type { IRequest } from '@auth/interface/request.interface';
 import type { IResponse } from '@common/interfaces/response.interface';
+import { Auth } from '@common/decorators/auth.decorator';
 import { AuthService } from '@auth/auth.service';
+import { ERole } from '@common/enums/role.enum';
 import { LoginDto } from '@auth/dto/login.dto';
 
 @Controller('auth')
@@ -9,7 +12,13 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() loginDto: LoginDto): Promise<IResponse<ILogin>> {
+  login(@Body() loginDto: LoginDto): Promise<IResponse<IPayload>> {
     return this.authService.login(loginDto);
+  }
+
+  @Auth([ERole.Admin, ERole.Super])
+  @Get('logout')
+  logout(@Req() req: IRequest): Promise<IResponse<IPayload>> {
+    return this.authService.logout(req);
   }
 }
