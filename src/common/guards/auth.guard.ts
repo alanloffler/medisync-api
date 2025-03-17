1f0ec84cbc7a176783cfc6b6d812ec31dcc64e13
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 11:14:42 -0300

    Add type annotation for JWT payload in AuthGuard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } 
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import type { IPayload } from '@auth/interface/payload.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -18,7 +19,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const secret: string = this.configService.get<string>('JWT_ACCESS_SECRET');
-      const payload = await this.jwtService.verifyAsync(tokenExists, { secret });
+      const payload: IPayload = await this.jwtService.verifyAsync(tokenExists, { secret });
       request.user = payload;
     } catch (error) {
       throw new HttpException('Unauthorized - Invalid token', HttpStatus.UNAUTHORIZED);
