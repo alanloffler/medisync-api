c0f1cc4c06d8bcd306fd8ebf6b6c90a9c5e84b19
alanloffler <alanmatiasloffler@gmail.com>
2025-03-19 10:37:31 -0300

    Add Passport and authentication strategies to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AdminModule } from '@admin/admin.module';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
+import { JwtStrategy } from '@auth/strategies/jwt.strategy';
+import { LocalStrategy } from '@auth/strategies/local.strategy';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, ConfigService, JwtService],
+  providers: [AuthService, ConfigService, JwtService, JwtStrategy, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
