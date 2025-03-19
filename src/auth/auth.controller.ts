78bbec96c51cc22a718cb52966ca7a7ede3cbda6
alanloffler <alanmatiasloffler@gmail.com>
2025-03-19 10:41:04 -0300

    Refactor auth controller to use Guards instead of decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,29 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import type { IPayload } from '@auth/interface/payload.interface';
 import type { IRequest } from '@auth/interface/request.interface';
 import type { IResponse } from '@common/interfaces/response.interface';
 import type { ITokens } from '@auth/interface/tokens.interface';
-import { Auth } from '@common/decorators/auth.decorator';
 import { AuthService } from '@auth/auth.service';
-import { ERole } from '@common/enums/role.enum';
-import { LoginDto } from '@auth/dto/login.dto';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from '@auth/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Body() loginDto: LoginDto): Promise<IResponse<IPayload>> {
-    return this.authService.login(loginDto);
+  login(@Req() req: IRequest): Promise<IResponse<IPayload>> {
+    return this.authService.loginWithCredentials(req.user);
   }
 
-  @Auth([ERole.Admin, ERole.Super])
+  @UseGuards(JwtAuthGuard)
   @Get('logout')
   logout(@Req() req: IRequest): Promise<IResponse<IPayload>> {
     return this.authService.logout(req.user);
   }
 
-  @Auth([ERole.Admin, ERole.Super])
+  @UseGuards(JwtAuthGuard)
   @Get('refreshTokens')
   refreshTokens(@Req() req: IRequest, @Body() tokens: ITokens): Promise<IResponse<IPayload>> {
     return this.authService.refreshTokens(req.user, tokens);
