ffe43c2865c2fe085e17e01e99eedacc9d461df4
alanloffler <alanmatiasloffler@gmail.com>
2025-03-19 17:27:50 -0300

    Update auth controller to use cookies for token handling Refactor auth endpoints to use HTTP-only cookies Controller methods now pass Response object to service methods and the refresh endpoint gets token from cookies rather than request body.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import type { IPayload } from '@auth/interface/payload.interface';
 import type { IRequest } from '@auth/interface/request.interface';
 import type { IResponse } from '@common/interfaces/response.interface';
-import type { ITokens } from '@auth/interface/tokens.interface';
 import { AuthService } from '@auth/auth.service';
 import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from '@auth/guards/local-auth.guard';
@@ -13,19 +12,22 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Req() req: IRequest): Promise<IResponse<IPayload>> {
-    return this.authService.loginWithCredentials(req.user);
+  login(@Req() req: IRequest, @Res({ passthrough: true }) res: Response): Promise<IResponse<IPayload>> {
+    return this.authService.loginWithCredentials(req.user, res);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('logout')
-  logout(@Req() req: IRequest): Promise<IResponse<IPayload>> {
-    return this.authService.logout(req.user);
+  logout(@Req() req: IRequest, @Res({ passthrough: true }) res: Response): Promise<IResponse<IPayload>> {
+    return this.authService.logout(req.user, res);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('refreshTokens')
-  refreshTokens(@Req() req: IRequest, @Body() tokens: ITokens): Promise<IResponse<IPayload>> {
-    return this.authService.refreshTokens(req.user, tokens);
+  // @UseGuards(JwtAuthGuard)
+  @Get('refresh')
+  refreshTokens(@Req() req: IRequest, @Res({ passthrough: true }) res: Response): Promise<IResponse<IPayload>> {
+    const refreshToken = req.cookies?.refresh_token;
+    if (!refreshToken) throw new HttpException('Refresh token is required', HttpStatus.BAD_REQUEST);
+
+    return this.authService.refreshTokens(req.user, refreshToken, res);
   }
 }
