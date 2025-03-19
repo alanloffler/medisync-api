17e05899694802957f3743bf3777f55667776ac3
alanloffler <alanmatiasloffler@gmail.com>
2025-03-19 18:59:22 -0300

    Add cookies property in request interface

--- src/auth/interface/request.interface.ts
+++ src/auth/interface/request.interface.ts
@@ -1,5 +1,11 @@
 import type { IPayload } from '@auth/interface/payload.interface';
+import type { Request } from 'express';
 
 export interface IRequest extends Request {
   user: IPayload;
+  cookies: {
+    [key: string]: string;
+    accessToken: string;
+    refreshToken: string;
+  };
 }
