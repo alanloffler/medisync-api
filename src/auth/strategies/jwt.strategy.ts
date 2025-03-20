9cb82f960c02601cd01e92548754759cf7b4ae5e
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 09:22:01 -0300

    Fix JWT cookie name from access_token to accessToken

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          return request?.cookies?.access_token;
+          return request?.cookies?.accessToken;
         },
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       ]),
