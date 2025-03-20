40409cc128908467e4966e5e24ea825531445045
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 15:29:49 -0300

    Sort strategies alphabetically in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,14 @@ import { PassportModule } from '@nestjs/passport';
 import { AdminModule } from '@admin/admin.module';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
-import { JwtStrategy } from '@auth/strategies/jwt.strategy';
 import { JwtRefreshStrategy } from '@auth/strategies/jwt-refresh.strategy';
+import { JwtStrategy } from '@auth/strategies/jwt.strategy';
 import { LocalStrategy } from '@auth/strategies/local.strategy';
 
 @Module({
   imports: [AdminModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, ConfigService, JwtService, JwtStrategy, JwtRefreshStrategy, LocalStrategy],
+  providers: [AuthService, ConfigService, JwtRefreshStrategy, JwtService, JwtStrategy, LocalStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
