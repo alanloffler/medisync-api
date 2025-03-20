778bb31dc8833e0176685258fff0c16e8fe3f4bd
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 13:31:01 -0300

    Add JwtRefreshStrategy to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,12 +6,13 @@ import { AdminModule } from '@admin/admin.module';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
 import { JwtStrategy } from '@auth/strategies/jwt.strategy';
+import { JwtRefreshStrategy } from '@auth/strategies/jwt-refresh.strategy';
 import { LocalStrategy } from '@auth/strategies/local.strategy';
 
 @Module({
   imports: [AdminModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, ConfigService, JwtService, JwtStrategy, LocalStrategy],
+  providers: [AuthService, ConfigService, JwtService, JwtStrategy, JwtRefreshStrategy, LocalStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
