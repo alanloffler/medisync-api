c0ea4cdfebeb5c3a915eeb92e87c896966ca1d00
alanloffler <alanmatiasloffler@gmail.com>
2025-03-22 22:35:32 -0300

    Add I18n module with language resolution configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AdminModule } from '@admin/admin.module';
 import { AppointmentsModule } from '@appointments/appointments.module';
 import { AreasModule } from '@areas/areas.module';
@@ -38,6 +40,18 @@ import { UsersModule } from '@users/users.module';
           expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
         },
       }),
+    }),
+    I18nModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage: configService.getOrThrow<string>('I18N_FALLBACK_LANGUAGE'),
+        loaderOptions: {
+          path: configService.get<string>('NODE_ENV') === 'development' ? join(process.cwd(), 'src/i18n/') : join(__dirname, '/i18n/'),
+          watch: true,
+        },
+      }),
+      resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver, new HeaderResolver(['x-lang'])],
     }),
     AdminModule,
     AppointmentsModule,
