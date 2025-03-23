dd38969f47679785e2c9c4709256827dc6ba16d9
alanloffler <alanmatiasloffler@gmail.com>
2025-03-22 22:35:11 -0300

    Add i18n support for authentication error messages

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { I18nService } from 'nestjs-i18n';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import type { IPayload } from '@auth/interface/payload.interface';
@@ -7,7 +8,10 @@ import { AuthService } from '@auth/auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly i18nService: I18nService,
+  ) {
     super({
       usernameField: 'email',
       passwordField: 'password',
@@ -16,7 +20,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   public async validate(email: string, password: string): Promise<IPayload> {
     const admin: Admin = await this.authService.validateAdmin(email, password);
-    if (!admin) throw new HttpException("Couldn't validate admin, invalid credentials (Local strategy)", HttpStatus.UNAUTHORIZED);
+    if (!admin) throw new HttpException(this.i18nService.t('exception.auth.login'), HttpStatus.UNAUTHORIZED);
 
     return {
       _id: admin._id,
