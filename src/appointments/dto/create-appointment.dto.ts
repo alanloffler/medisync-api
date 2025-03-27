8f4781cf9896832c16d38b73fa3cd897a3238b7e
alanloffler <alanmatiasloffler@gmail.com>
2025-03-27 19:56:14 -0300

    Replace hardcoded validation messages with i18n translations

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,28 +1,29 @@
 import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
-import { APPOINTMENTS_CONFIG } from '@config/appointments.config';
 import { EStatus } from '@common/enums/status.enum';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import { I18nTranslations } from '@i18n/i18n.generated';
 
 export class CreateAppointmentDto {
-  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.professional })
-  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.professional })
+  @IsNotEmpty({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.professional.isNotEmpty') })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.professional.isString') })
   professional: string;
 
-  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.user })
-  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.user })
+  @IsNotEmpty({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.user.isNotEmpty') })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.user.isString') })
   user: string;
 
-  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.day })
-  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.day })
+  @IsNotEmpty({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.day.isNotEmpty') })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.day.isString') })
   day: string;
 
-  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.hour })
-  @IsNotEmpty({ message: APPOINTMENTS_CONFIG.validation.isNotEmpty.hour })
+  @IsNotEmpty({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.hour.isNotEmpty') })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.hour.isString') })
   hour: string;
 
-  @IsInt({ message: APPOINTMENTS_CONFIG.validation.isInt.slot })
+  @IsInt({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.slot.isInt') })
   slot: number;
 
   @IsOptional()
-  @IsString({ message: APPOINTMENTS_CONFIG.validation.isString.status })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.appointments.status.isString') })
   status: EStatus;
 }
