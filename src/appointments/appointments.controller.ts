4471488208641109e559967ab07b6dfc861869a8
alanloffler <alanmatiasloffler@gmail.com>
2025-03-28 16:17:02 -0300

    Add proper return types and clean unused endpoints

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,106 +1,118 @@
 import { Controller, Get, Post, Body, Param, Delete, Query, Patch } from '@nestjs/common';
 import type { IAppoAttendance } from './interfaces/appo-attendance.interface';
+import type { IApposDays } from '@appointments/interfaces/appos-days.interface';
 import type { IResponse } from '@common/interfaces/response.interface';
 import { Appointment } from '@appointments/schema/appointment.schema';
 import { AppointmentsService } from '@appointments/appointments.service';
 import { CreateAppointmentDto } from '@appointments/dto/create-appointment.dto';
+import { Professional } from '@professionals/schema/professional.schema';
+import { IStatistic } from '@common/interfaces/statistic.interface';
 
 @Controller('appointments')
 export class AppointmentsController {
   constructor(private readonly appointmentsService: AppointmentsService) {}
-  // CHECKED: used on DialogReserve.tsx
+
+  // * CHECKED: used on Frontend
   @Post()
   create(@Body() createAppointmentDto: CreateAppointmentDto): Promise<IResponse<Appointment>> {
     return this.appointmentsService.create(createAppointmentDto);
   }
-  // CHECKED: used on appointments data table
+
+  // * CHECKED: used on Frontend
   @Post('search')
   findSearch(@Body() dto: any): Promise<IResponse<Appointment[]>> {
     return this.appointmentsService.findSearch(dto);
   }
 
-  @Get()
-  findAll(@Query('p') page: string, @Query('l') limit: string): Promise<IResponse<Appointment[]>> {
-    return this.appointmentsService.findAll(page, limit);
-  }
+  // @Get()
+  // findAll(@Query('p') page: string, @Query('l') limit: string): Promise<IResponse<Appointment[]>> {
+  //   return this.appointmentsService.findAll(page, limit);
+  // }
 
+  // * CHECKED: used on Frontend
   @Get('byProfessional')
-  findAllByProfessional(@Query('id') id: string, @Query('day') day: string): Promise<IResponse> {
+  findAllByProfessional(@Query('id') id: string, @Query('day') day: string): Promise<IResponse<Appointment[]>> {
     return this.appointmentsService.findAllByProfessional(id, day);
   }
 
-  @Get('byUser')
-  findAllByUser(@Query('id') id: string): Promise<IResponse> {
-    return this.appointmentsService.findAllByUser(id);
-  }
+  // @Get('byUser')
+  // findAllByUser(@Query('id') id: string): Promise<IResponse> {
+  //   return this.appointmentsService.findAllByUser(id);
+  // }
 
+  // * CHECKED: used on Frontend
   @Get('uniqueProfessionalsByUser')
-  findUniqueProfessionalsByUser(@Query('u') id: string): Promise<IResponse> {
+  findUniqueProfessionalsByUser(@Query('u') id: string): Promise<IResponse<Professional[]>> {
     return this.appointmentsService.findUniqueProfessionalsByUser(id);
   }
 
-  // CHECKED: used in ApposTable.tsx
+  // * CHECKED: used on Frontend
   @Get('byFilters')
-  findApposRecordWithFilters(@Query('u') userId: string, @Query('l') limit?: string, @Query('pg') page?: string, @Query('p') professionalId?: string, @Query('y') year?: string): Promise<IResponse> {
+  findApposRecordWithFilters(@Query('u') userId: string, @Query('l') limit?: string, @Query('pg') page?: string, @Query('p') professionalId?: string, @Query('y') year?: string): Promise<IResponse<Appointment[]>> {
     return this.appointmentsService.findApposRecordWithFilters(userId, limit, page, professionalId, year);
   }
-  // CHECKED: used on appointments data table
+
+  // * CHECKED: used on Frontend
   @Get('count')
-  countAppointments(): Promise<IResponse> {
+  countAppointments(): Promise<IResponse<number>> {
     return this.appointmentsService.countAppointments();
   }
-  // CHECKED: used on DateSelection.tsx
+
+  // * CHECKED: used on Frontend
   @Get('daysWithAppos')
-  daysWithAppos(@Query('professionalId') professionalId: string, @Query('year') year: string, @Query('month') month: string): Promise<IResponse> {
+  daysWithAppos(@Query('professionalId') professionalId: string, @Query('year') year: string, @Query('month') month: string): Promise<IResponse<IApposDays[]>> {
     return this.appointmentsService.daysWithAppos(professionalId, year, month);
   }
-  // CHECKED: used on ApposFlowCard
+
+  // * CHECKED: used on Frontend
   @Get('statistics')
-  getApposStatistics(): Promise<IResponse> {
+  getApposStatistics(): Promise<IResponse<IStatistic[]>> {
     return this.appointmentsService.getApposStatistics();
   }
 
-  // CHECKED: used on ApposAttendance.tsx
+  // * CHECKED: used on Frontend
   @Get('attendance')
   getAttendance(): Promise<IResponse<IAppoAttendance[]>> {
     return this.appointmentsService.getAttendance();
   }
 
   // FIXME: both methods will be replaced by an unified method (previous method)
-  @Get('byUserAndProfessional')
-  findAllByUserAndProfessional(@Query('user') userId: string, @Query('professional') professionalId: string): Promise<IResponse> {
-    return this.appointmentsService.findAllByUserAndProfessional(userId, professionalId);
-  }
+  // @Get('byUserAndProfessional')
+  // findAllByUserAndProfessional(@Query('user') userId: string, @Query('professional') professionalId: string): Promise<IResponse> {
+  //   return this.appointmentsService.findAllByUserAndProfessional(userId, professionalId);
+  // }
 
-  @Get('byUserAndYear')
-  findAllByUserAndYear(@Query('u') user: string, @Query('y') year: string, @Query('m') month: string): Promise<IResponse> {
-    return this.appointmentsService.findAllByUserAndYear(user, year, month);
-  }
+  // @Get('byUserAndYear')
+  // findAllByUserAndYear(@Query('u') user: string, @Query('y') year: string, @Query('m') month: string): Promise<IResponse> {
+  //   return this.appointmentsService.findAllByUserAndYear(user, year, month);
+  // }
 
+  // * CHECKED: used on Frontend
   @Get('yearsByUser')
-  findApposYearsByUser(@Query('u') user: string): Promise<IResponse> {
+  findApposYearsByUser(@Query('u') user: string): Promise<IResponse<string[]>> {
     return this.appointmentsService.findApposYearsByUser(user);
   }
 
-  @Get('monthsByUser')
-  findApposMonthsByUser(@Query('u') user: string, @Query('y') year: string): Promise<IResponse> {
-    return this.appointmentsService.findApposMonthsByUser(user, year);
-  }
+  // @Get('monthsByUser')
+  // findApposMonthsByUser(@Query('u') user: string, @Query('y') year: string): Promise<IResponse> {
+  //   return this.appointmentsService.findApposMonthsByUser(user, year);
+  // }
 
+  // * CHECKED: used on Frontend
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<IResponse> {
+  findOne(@Param('id') id: string): Promise<IResponse<Appointment>> {
     return this.appointmentsService.findOne(id);
   }
 
-  // CHECKED:
-  // Used on StatusSelect.tsx and AppoDataTable.tsx (change professional)
+  // * CHECKED: used on Frontend
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: { professional: string; status: string }): Promise<IResponse<Appointment>> {
     return this.appointmentsService.update(id, dto);
   }
-  // CHECKED: used on DialogReserve.tsx
+
+  // * CHECKED: used on Frontend
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<IResponse> {
+  remove(@Param('id') id: string): Promise<IResponse<Appointment>> {
     return this.appointmentsService.remove(id);
   }
 }
