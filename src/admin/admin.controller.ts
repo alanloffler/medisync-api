fafc1b55ea73711e9c78dbebd1b5c52959181fdb
alanloffler <alanmatiasloffler@gmail.com>
2025-03-29 11:17:23 -0300

    Add auth guards and role-based access control to admin routes

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import type { IResponse } from '@common/interfaces/response.interface';
 import { Admin } from '@admin/schema/admin.schema';
 import { AdminService } from '@admin/admin.service';
 import { CreateAdminDto } from '@admin/dto/create-admin.dto';
+import { ERole } from '@common/enums/role.enum';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { Roles } from '@common/decorators/roles.decorator';
+import { RolesGuard } from '@auth/guards/roles.guard';
 import { UpdateAdminDto } from '@admin/dto/update-admin.dto';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles([ERole.Super, ERole.Admin])
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -29,6 +35,7 @@ export class AdminController {
     return this.adminService.update(id, updateAdminDto);
   }
 
+  @Roles([ERole.Super])
   @Delete(':id')
   remove(@Param('id') id: string): Promise<IResponse<Admin>> {
     return this.adminService.remove(id);
