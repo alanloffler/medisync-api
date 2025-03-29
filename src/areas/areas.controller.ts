2d171a4f514ecf975dd19aab5ffde002670cf817
alanloffler <alanmatiasloffler@gmail.com>
2025-03-29 13:13:06 -0300

    Add JWT and role-based authentication to areas controller

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -1,40 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import type { IResponse } from '@common/interfaces/response.interface';
+import { Area } from '@areas/schema/areas.schema';
 import { AreasService } from '@areas/areas.service';
 import { CreateAreaDto } from '@areas/dto/create-area.dto';
-import { IResponse } from '@common/interfaces/response.interface';
+import { ERole } from '@common/enums/role.enum';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { Roles } from '@common/decorators/roles.decorator';
+import { RolesGuard } from '@auth/guards/roles.guard';
 import { UpdateAreaDto } from '@areas/dto/update-area.dto';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles([ERole.Super, ERole.Admin])
 @Controller('areas')
 export class AreasController {
   constructor(private readonly areasService: AreasService) {}
 
   @Post()
-  create(@Body() createAreaDto: CreateAreaDto): Promise<IResponse> {
+  create(@Body() createAreaDto: CreateAreaDto): Promise<IResponse<Area>> {
     return this.areasService.create(createAreaDto);
   }
 
   @Get()
-  findAll(): Promise<IResponse> {
+  findAll(): Promise<IResponse<Area[]>> {
     return this.areasService.findAll();
   }
 
   @Get('/active')
-  findAllActive(): Promise<IResponse> {
+  findAllActive(): Promise<IResponse<Area[]>> {
     return this.areasService.findAllActive();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<IResponse> {
+  findOne(@Param('id') id: string): Promise<IResponse<Area>> {
     return this.areasService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAreaDto: UpdateAreaDto): Promise<IResponse> {
+  update(@Param('id') id: string, @Body() updateAreaDto: UpdateAreaDto): Promise<IResponse<Area>> {
     return this.areasService.update(id, updateAreaDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<IResponse> {
+  remove(@Param('id') id: string): Promise<IResponse<Area>> {
     return this.areasService.remove(id);
   }
 }
