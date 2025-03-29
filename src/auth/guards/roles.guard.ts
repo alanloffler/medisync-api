a0144636b9a7f355e744d4725de34b9207f4f4c5
alanloffler <alanmatiasloffler@gmail.com>
2025-03-29 12:05:49 -0300

    Add i18n to RolesGuard for localized error messages

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,10 +1,15 @@
 import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { I18nService } from 'nestjs-i18n';
 import { Reflector } from '@nestjs/core';
+import type { I18nTranslations } from '@i18n/i18n.generated';
 import { ERole } from '@common/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly i18nService: I18nService<I18nTranslations>,
+    private readonly reflector: Reflector,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles: ERole[] = this.reflector.getAllAndOverride<ERole[]>('roles', [context.getHandler(), context.getClass()]);
@@ -15,7 +20,7 @@ export class RolesGuard implements CanActivate {
     const roleExists: boolean = requiredRoles.some((role) => user.role === role);
 
     if (!roleExists) {
-      throw new HttpException('Unauthorized - Credentials are not valid for role needed', HttpStatus.UNAUTHORIZED);
+      throw new HttpException(this.i18nService.t('exception.auth.forbidden'), HttpStatus.FORBIDDEN);
     } else {
       return true;
     }
