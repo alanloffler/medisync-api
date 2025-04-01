df62e94f3e6a9a2ae34e43bd86219af13a9d9ba3
alanloffler <alanmatiasloffler@gmail.com>
2025-04-01 14:19:53 -0300

    Comment out email validation config

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -7,13 +7,13 @@ export const EMAIL_CONFIG = {
       send: 'Email enviado con éxito',
     },
   },
-  validation: {
-    isEmail: {
-      to: 'Cada contenido del recipiente debe ser un email',
-    },
-    isString: {
-      body: 'El contenido debe ser una cadena de texto',
-      subject: 'El sujeto debe ser una cadena de texto',
-    },
-  },
+  // validation: {
+  //   isEmail: {
+  //     to: 'Cada contenido del recipiente debe ser un email',
+  //   },
+  //   isString: {
+  //     body: 'El contenido debe ser una cadena de texto',
+  //     subject: 'El sujeto debe ser una cadena de texto',
+  //   },
+  // },
 };
