104c943a403c339aace2cbc8efbdef9b4a6c6f66
alanloffler <alanmatiasloffler@gmail.com>
2025-04-01 16:14:12 -0300

    Implement update and delete functionality for titles. Typed responses

--- src/titles/titles.controller.ts
+++ src/titles/titles.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import type { IResponse } from '@common/interfaces/response.interface';
 import { CreateTitleDto } from '@titles/dto/create-title.dto';
-import { IResponse } from '@common/interfaces/response.interface';
+import { Title } from '@titles/schema/title.schema';
 import { TitlesService } from '@titles/titles.service';
 import { UpdateTitleDto } from '@titles/dto/update-title.dto';
 
@@ -9,27 +10,27 @@ export class TitlesController {
   constructor(private readonly titlesService: TitlesService) {}
 
   @Post()
-  create(@Body() createTitleDto: CreateTitleDto): Promise<IResponse> {
+  create(@Body() createTitleDto: CreateTitleDto): Promise<IResponse<Title>> {
     return this.titlesService.create(createTitleDto);
   }
 
   @Get()
-  findAll(): Promise<IResponse> {
+  findAll(): Promise<IResponse<Title[]>> {
     return this.titlesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<IResponse> {
+  findOne(@Param('id') id: string): Promise<IResponse<Title>> {
     return this.titlesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTitleDto: UpdateTitleDto): Promise<IResponse> {
+  update(@Param('id') id: string, @Body() updateTitleDto: UpdateTitleDto): Promise<IResponse<Title>> {
     return this.titlesService.update(id, updateTitleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<IResponse> {
+  remove(@Param('id') id: string): Promise<IResponse<Title>> {
     return this.titlesService.remove(id);
   }
 }
--- src/titles/titles.service.ts
+++ src/titles/titles.service.ts
@@ -1,8 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import type { IResponse } from '@common/interfaces/response.interface';
 import { CreateTitleDto } from '@titles/dto/create-title.dto';
-import { IResponse } from '@common/interfaces/response.interface';
 import { TITLES_CONFIG } from '@config/titles.config';
 import { Title } from '@titles/schema/title.schema';
 import { UpdateTitleDto } from '@titles/dto/update-title.dto';
@@ -11,32 +11,59 @@ import { UpdateTitleDto } from '@titles/dto/update-title.dto';
 export class TitlesService {
   constructor(@InjectModel(Title.name) private readonly titleModel: Model<Title>) {}
 
-  async create(createTitleDto: CreateTitleDto): Promise<IResponse> {
+  async create(createTitleDto: CreateTitleDto): Promise<IResponse<Title>> {
     const title = await this.titleModel.create(createTitleDto);
     if (!title) throw new HttpException(TITLES_CONFIG.response.error.notCreated, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: TITLES_CONFIG.response.success.created, data: title };
+    return {
+      data: title,
+      message: TITLES_CONFIG.response.success.created,
+      statusCode: HttpStatus.OK,
+    };
   }
 
   async findAll(): Promise<IResponse<Title[]>> {
     const titles = await this.titleModel.find().sort({ name: 'asc' });
-    if (!titles) throw new HttpException(TITLES_CONFIG.response.error.notFoundPlural, HttpStatus.BAD_REQUEST);
+    if (titles.length === 0) throw new HttpException('', HttpStatus.NOT_FOUND);
+    if (titles === undefined || titles === null) throw new HttpException(TITLES_CONFIG.response.error.notFoundPlural, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: TITLES_CONFIG.response.success.foundPlural, data: titles };
+    return {
+      data: titles,
+      message: TITLES_CONFIG.response.success.foundPlural,
+      statusCode: HttpStatus.OK,
+    };
   }
 
-  async findOne(id: string): Promise<IResponse> {
+  async findOne(id: string): Promise<IResponse<Title>> {
     const title = await this.titleModel.findById(id);
     if (!title) throw new HttpException(TITLES_CONFIG.response.error.notFoundSingular, HttpStatus.BAD_REQUEST);
 
-    return { statusCode: 200, message: TITLES_CONFIG.response.success.foundSingular, data: title };
+    return {
+      data: title,
+      message: TITLES_CONFIG.response.success.foundSingular,
+      statusCode: HttpStatus.OK,
+    };
   }
-  // TODO: add functionality to update title
-  async update(id: string, updateTitleDto: UpdateTitleDto): Promise<IResponse> {
-    return { statusCode: 200, message: TITLES_CONFIG.response.success.updated, data: [id, updateTitleDto] };
+
+  async update(id: string, updateTitleDto: UpdateTitleDto): Promise<IResponse<Title>> {
+    const title = await this.titleModel.findByIdAndUpdate(id, updateTitleDto, { new: true });
+    if (!title) throw new HttpException('update ok', HttpStatus.BAD_REQUEST);
+
+    return {
+      data: title,
+      message: TITLES_CONFIG.response.success.updated,
+      statusCode: HttpStatus.OK,
+    };
   }
-  // TODO: add functionality to remove title
-  async remove(id: string): Promise<IResponse> {
-    return { statusCode: 200, message: TITLES_CONFIG.response.success.removed, data: id };
+
+  async remove(id: string): Promise<IResponse<Title>> {
+    const title = await this.titleModel.findByIdAndDelete(id);
+    if (!title) throw new HttpException('Removed not ok', HttpStatus.BAD_REQUEST);
+
+    return {
+      data: title,
+      message: TITLES_CONFIG.response.success.removed,
+      statusCode: HttpStatus.OK,
+    };
   }
 }
