e294a3183f978a70ce150177b585e9a36cef3da9
alanloffler <alanmatiasloffler@gmail.com>
2025-04-01 19:06:14 -0300

    Add auth guards and role restrictions to statistics controller

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import type { IResponse } from '@common/interfaces/response.interface';
 import type { IStats } from '@statistics/interfaces/statistics.interface';
+import { ERole } from '@common/enums/role.enum';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { Roles } from '@auth/decorators/roles.decorator';
+import { RolesGuard } from '@auth/guards/roles.guard';
 import { StatisticsService } from '@statistics/statistics.service';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles([ERole.Super, ERole.Admin])
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
