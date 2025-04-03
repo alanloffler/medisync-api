52ad2c535b429cd54dbef967117c1843b2ff29ac
alanloffler <alanmatiasloffler@gmail.com>
2025-04-03 16:51:25 -0300

    Add explicit _id field to User schema with auto generation

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -1,10 +1,16 @@
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    auto: true,
+  })
+  _id: string;
+
   @Prop({ lowercase: true, trim: true, required: true })
   firstName: string;
 
