de12629f26750d89941573361e7946209efaf81a
alanloffler <alanmatiasloffler@gmail.com>
2025-04-02 21:17:20 -0300

    Replace hardcoded validation messages with i18n translations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,33 +1,34 @@
 import { IsEmail, IsInt, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
-import { USERS_CONFIG } from '@config/users.config';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import type { I18nTranslations } from '@i18n/i18n.generated';
 
 export class CreateUserDto {
-  @IsString({ message: USERS_CONFIG.validation.isString.firstName })
-  @MinLength(3, { message: USERS_CONFIG.validation.min.firstName })
-  @MaxLength(30, { message: USERS_CONFIG.validation.max.firstName })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.users.firstName.isString') })
+  @MinLength(3, { message: i18nValidationMessage<I18nTranslations>('validation.users.firstName.minLength') })
+  @MaxLength(30, { message: i18nValidationMessage<I18nTranslations>('validation.users.firstName.maxLength') })
   firstName: string;
 
-  @IsString({ message: USERS_CONFIG.validation.isString.lastName })
-  @MinLength(3, { message: USERS_CONFIG.validation.min.lastName })
-  @MaxLength(30, { message: USERS_CONFIG.validation.max.lastName })
+  @IsString({ message: i18nValidationMessage<I18nTranslations>('validation.users.lastName.isString') })
+  @MinLength(3, { message: i18nValidationMessage<I18nTranslations>('validation.users.lastName.minLength') })
+  @MaxLength(30, { message: i18nValidationMessage<I18nTranslations>('validation.users.lastName.maxLength') })
   lastName: string;
 
-  @IsInt({ message: USERS_CONFIG.validation.isInt.dni })
-  @Min(1000000, { message: USERS_CONFIG.validation.min.dni }) // 1 mill min
-  @Max(99999999, { message: USERS_CONFIG.validation.max.dni }) // 99.99 mill max
+  @IsInt({ message: i18nValidationMessage<I18nTranslations>('validation.users.dni.isInt') })
+  @Min(1000000, { message: i18nValidationMessage<I18nTranslations>('validation.users.dni.min') }) // 1 mill min
+  @Max(99999999, { message: i18nValidationMessage<I18nTranslations>('validation.users.dni.max') }) // 99.99 mill max
   dni: number;
 
-  @IsInt({ message: USERS_CONFIG.validation.isInt.areaCode })
-  @Min(1, { message: USERS_CONFIG.validation.min.areaCode }) // 1
-  @Max(999, { message: USERS_CONFIG.validation.max.areaCode }) // 000
+  @IsInt({ message: i18nValidationMessage<I18nTranslations>('validation.users.areaCode.isInt') })
+  @Min(1, { message: i18nValidationMessage<I18nTranslations>('validation.users.areaCode.min') }) // 1
+  @Max(999, { message: i18nValidationMessage<I18nTranslations>('validation.users.areaCode.max') }) // 000
   areaCode: number;
 
-  @IsInt({ message: USERS_CONFIG.validation.isInt.phone })
-  @Min(1000000000, { message: USERS_CONFIG.validation.min.phone }) // 0000 000000
-  @Max(9999999999, { message: USERS_CONFIG.validation.max.phone }) // 0000 000000
+  @IsInt({ message: i18nValidationMessage<I18nTranslations>('validation.users.phone.isInt') })
+  @Min(1000000000, { message: i18nValidationMessage<I18nTranslations>('validation.users.phone.min') }) // 0000 000000
+  @Max(9999999999, { message: i18nValidationMessage<I18nTranslations>('validation.users.phone.max') }) // 0000 000000
   phone: number;
 
   @IsOptional()
-  @IsEmail({}, { message: USERS_CONFIG.validation.isEmail.email })
+  @IsEmail({}, { message: i18nValidationMessage<I18nTranslations>('validation.users.email.isEmail') })
   email?: string;
 }
